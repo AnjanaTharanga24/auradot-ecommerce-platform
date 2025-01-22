33dfc01ff47d81dae8be35dfd0ccdca09fb2c11f
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-01-22 15:27:21 +0530

    create seller dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { ItemListComponent } from './item-list/item-list.component';
+import { SellerComponent } from './seller/seller.component';
 
 export const routes: Routes = [
     { path: '', component: ItemListComponent },
+    { path: 'seller-dashboard', component: SellerComponent },
+
 ];
