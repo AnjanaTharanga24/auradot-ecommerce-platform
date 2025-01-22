1d7162c0e2feeb55a797b2452a6299c3592a35ab
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-01-22 12:05:13 +0530

    create business logic for add items

---
+++ backend/src/main/java/com/auradot/backend/model/Item.java
@@ -0,0 +1,17 @@
+package com.auradot.backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Float price;
+
+}
