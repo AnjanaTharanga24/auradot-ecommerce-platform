1d0a71da997976c571ad20fcd7f1e6c8fe681e20
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-01-28 16:34:13 +0530

    connect update quantity function

--- frontend/src/app/buyer/buyer.component.ts
+++ frontend/src/app/buyer/buyer.component.ts
@@ -25,4 +25,37 @@ export class BuyerComponent {
     this.getCartItems()
   }
 
+  async updateQuantity(itemId: number, newQuantity: number) {
+    try {
+      const updateQuantityRequest = { quantity: newQuantity };
+      const response = await axios.put(
+        `http://localhost:8080/api/buyer/items/${itemId}`,
+        updateQuantityRequest
+      );
+      console.log('Quantity updated:', response.data);
+      this.getCartItems(); 
+    } catch (error) {
+      console.error('Error updating quantity:', error);
+    }
+  }
+  
+  handleIncrease(item: any) {
+    if (item.quantity < 10) {
+      item.quantity++;
+      this.updateQuantity(item.id, item.quantity);
+    } else {
+      alert('You can only add up to 10 items');
+    }
+  }
+  
+  handleDecrease(item: any) {
+    if (item.quantity > 0) {
+      item.quantity--;
+      this.updateQuantity(item.id, item.quantity);
+    } else {
+      alert("Quantity can't be less than 0");
+    }
+  }
+  
+
 }
