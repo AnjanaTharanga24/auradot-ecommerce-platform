d97443d611b29406a1e82786e1ff48f6cdcd2014
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-01-28 11:04:34 +0530

    connect getAllItems() to frontend

--- frontend/src/app/item-list/item-list.component.ts
+++ frontend/src/app/item-list/item-list.component.ts
@@ -1,13 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-item-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './item-list.component.html',
   styleUrl: './item-list.component.css'
 })
 export class ItemListComponent {
-  
+   items : any[] = [];
+  async getAllItems(){
+    try {
+      const response = await axios.get(`http://localhost:8080/api/items/`);
+      this.items = response.data;
+      console.log(response.data);
+    } catch (error) {
+      console.log("Error fetching items : " + error);
+    }
+  }
+
+  ngOnInit(){
+    this.getAllItems();
+  }
+
 }
