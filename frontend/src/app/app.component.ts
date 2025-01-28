a9c17f9724657080311910acd125732b5a049f86
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-01-28 08:16:30 +0530

    connect backend implementation for insert data

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/seller/seller.component.ts
+++ frontend/src/app/seller/seller.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-seller',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './seller.component.html',
-  styleUrl: './seller.component.css'
+  styleUrls: ['./seller.component.css']
 })
 export class SellerComponent {
+  item = {
+    name: '',
+    description: '',
+    category: '',
+    price: 0,
+  };
 
+  async onSubmit() {
+    try {
+      const response = await axios.post(`http://localhost:8080/api/items/`, this.item);
+      console.log(response.data);
+      Swal.fire({
+        title: 'Success!',
+        text: 'Item added successfully!',
+        icon: 'success',
+        confirmButtonText: 'OK',
+        background: '#f0f9ff', 
+        color: '#4caf50', 
+        confirmButtonColor: '#4caf50', 
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
