9d1754b7da767a15ae505c0a5d4514f2e923c2e0
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-01-31 03:27:54 +0530

    add RouterLink

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { DemoAngularMaterailModule } from './DemoAngularMaterialModule';
 import { HttpClientModule } from '@angular/common/http';
 import { CartService } from './cart.service';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { SellerComponent } from "./seller/seller.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FormsModule,CommonModule, MatToolbarModule],
+  imports: [RouterOutlet,RouterLink, FormsModule, CommonModule, MatToolbarModule, SellerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
