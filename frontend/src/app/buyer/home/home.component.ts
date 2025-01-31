dfa0d1b408925026141195e6f58dbbdfee81efe6
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-01-31 08:35:45 +0530

    new update on the item-card and home page

---
+++ frontend/src/app/buyer/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { ItemCardComponent } from "../../components/item-card/item-card.component";
+import { ItemService } from '../../services/item.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, ItemCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  items: any[] = [];
+
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit() {
+    this.fetchItems();
+  }
+
+  async fetchItems() {
+    try {
+      this.items = await this.itemService.getAllItems();
+    } catch (error) {
+      console.error('Error fetching items:', error);
+    }
+  }
+}
