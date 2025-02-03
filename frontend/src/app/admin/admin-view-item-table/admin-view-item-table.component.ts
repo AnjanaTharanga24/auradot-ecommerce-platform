a701252e286e05732ac34d2ed11807cfb1ff6b02
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-02-03 09:30:16 +0530

    new update on the item service

---
+++ frontend/src/app/admin/admin-view-item-table/admin-view-item-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view-item-table',
+  imports: [],
+  templateUrl: './admin-view-item-table.component.html',
+  styleUrl: './admin-view-item-table.component.css'
+})
+export class AdminViewItemTableComponent {
+
+}
