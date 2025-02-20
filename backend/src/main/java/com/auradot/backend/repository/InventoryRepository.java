c3f4191fc32c1d458425cded8fbac2e624b4a0b4
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-02-20 15:56:18 +0530

    create logic for update item by id

--- backend/src/main/java/com/auradot/backend/repository/InventoryRepository.java
+++ backend/src/main/java/com/auradot/backend/repository/InventoryRepository.java
@@ -5,7 +5,9 @@ import com.auradot.backend.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory,Long> {
 
+    Optional<Inventory> findByItem_Id(Long id);
 }
