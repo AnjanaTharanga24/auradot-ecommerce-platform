55098a33170ae75fea6278a1e901c8812bd4027e
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-03-03 10:31:07 +0530

    complete search function

--- frontend/src/app/buyer/home/home.component.ts
+++ frontend/src/app/buyer/home/home.component.ts
@@ -1,28 +1,54 @@
 import { Component } from '@angular/core';
-import { ItemCardComponent } from "../../buyer/item-card/item-card.component";
+import { ItemCardComponent } from '../../buyer/item-card/item-card.component';
 import { CommonModule } from '@angular/common';
-import { SearchBarComponent } from "../search-bar/search-bar.component";
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { ItemService } from '../../services/item-service/item.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, ItemCardComponent, SearchBarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
   items: any[] = [];
+  theKeyword: string = '';
 
-  constructor(private itemService: ItemService) {}
+  constructor(
+    private itemService: ItemService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   async ngOnInit() {
+    this.theKeyword = this.route.snapshot.paramMap.get('name')!;
+
+    if (this.theKeyword) {
+      this.handleSearch();
+    } else {
+      this.getAllItems();
+    }
+  }
+
+  async getAllItems() {
     try {
       this.items = await this.itemService.getAllItems();
       console.log('Fetched items:', this.items);
     } catch (error) {
       console.error('Error fetching items:', error);
+    }
+  }
+
+  async handleSearch() {
+    if (this.theKeyword) {
+      try {
+        this.items = await this.itemService.searchItems(this.theKeyword);
+        console.log('Fetched items:', this.items);
+      } catch (error) {
+        console.error('Error fetching items:', error);
+      }
     }
   }
 }
--- frontend/src/app/buyer/item-card/item-card.component.ts
+++ frontend/src/app/buyer/item-card/item-card.component.ts
@@ -1,26 +1,33 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemService } from '../../services/item-service/item.service';
 import { CustomerService } from '../../services/customer.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './item-card.component.html',
-  styleUrl: './item-card.component.css'
+  styleUrl: './item-card.component.css',
 })
-export class ItemCardComponent {
-onAddToCart() {
- this.addToCart(this.item.id);
-}
+export class ItemCardComponent implements OnInit {
+
+
+  onAddToCart() {
+    this.addToCart(this.item.id);
+  }
+
   @Input() item: any;
 
-  constructor(private itemService: ItemService, private customerService: CustomerService) {}
+  constructor(
+    private itemService: ItemService,
+    private customerService: CustomerService,
+   
+  ) {}
 
   getStockStatusClass(status: string): string {
-    switch(status) {
+    switch (status) {
       case 'available':
         return 'status-available';
       case 'low_stock':
@@ -34,15 +41,15 @@ onAddToCart() {
 
   handleIncrease(item: any) {
     if (item.quantity < 10) {
-      item.quantity = item.quantity + 1; 
+      item.quantity = item.quantity + 1;
     } else {
-      alert("You can only add up to 10 items");
+      alert('You can only add up to 10 items');
     }
   }
-  
+
   handleDecrease(item: any) {
     if (item.quantity > 0) {
-      item.quantity = item.quantity - 1; 
+      item.quantity = item.quantity - 1;
     } else {
       alert("Quantity can't be less than 0");
     }
@@ -55,4 +62,9 @@ onAddToCart() {
       console.log('Error adding product:', error);
     }
   }
+
+  ngOnInit() {
+  }
+
+ 
 }
